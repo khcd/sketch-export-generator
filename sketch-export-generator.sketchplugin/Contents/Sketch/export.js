17cd88a8907badb00d2686fbd6dc04352f79df44
Kang Chen <kang@airtasker.com>
2016-11-08 13:23:47 +1100

    still trying to get format to work

--- sketch-export-generator.sketchplugin/Contents/Sketch/export.js
+++ sketch-export-generator.sketchplugin/Contents/Sketch/export.js
@@ -28,30 +28,20 @@ function setScale(scales) {
   SCALES = scales;
 }
 
-function prepareExportSizes(layer) {
+function prepareExportSizes(layer, format) {
   //log('=== ' + layer.name() + ' ===');
   //log('Remove all export sizes of layer \'' + layer.name() + '\'.');
   var sizes = layer.exportOptions().exportFormats();
   layer.exportOptions().removeAllExportFormats();
 
   SCALES.forEach(function(scale) {
-    addExportSize(layer, scale.size, scale.suffix);
+    addExportSize(layer, scale.size, scale.suffix, format);
   });
-
-  // for each (var scale in SCALES) {
-  //   addExportSize(layer, scale.size, scale.suffix);
-  // }
-
-  // for (var i = 0; i < SCALES.length; i++) {
-  //   scale = SCALES[i];
-  //   addExportSize(layer, scale.size, scale.suffix);
-  // }
 
   // add and remove a new export size to refresh UI
   layer.exportOptions().addExportFormat();
   layer.exportOptions().removeExportFormat(layer.exportOptions().exportFormats().objectAtIndex(layer.exportOptions().exportFormats().count() -1));
 }
-
 
 function addExportSize(layer, scale, suffix, format) {
 
@@ -59,14 +49,27 @@ function addExportSize(layer, scale, suffix, format) {
     format = "png";
   }
   //log('Add \'' + layer.name() + suffix + '\' (' + scale + 'h' + ')');
-
   var size = layer.exportOptions().addExportFormat();
-  //log('size');
-  //log(size);
+  // var size = layer.exportOptions()
+  log('*************** size **************** ');
+  log(layer);
+  log(layer.format);
+  log(layer.exportOptions().format);
+  log(layer.exportOptions().addExportFormat().format);
+  log('*************** size **************** ');
+
+  log(size);
   size.setName(suffix);
-  //log('set scale.');
-  //log(scale);
+  log('set scale.');
+  log(scale);
   size.setScale(scale);
+
+	// size.scale = scale;
+	// size.name = suffix;
+	// size.format = "SVG";
+  // log(size.format);
+
+
   //size.setVisibleScaleType(2); // e.g. 512h
 }
 
@@ -78,7 +81,15 @@ function runExportAndroid(context) {
   runExport(context);
 }
 
-function runExport(context) {
+function runExportPDF(context) {
+  runExport(context, "pdf");
+}
+
+function runExportSVG(context) {
+  runExport(context, "svg");
+}
+
+function runExport(context, format) {
 
   var selectedLayers = context.selection;
   var selectedCount = selectedLayers.count();
@@ -89,16 +100,27 @@ function runExport(context) {
   }
 
   // use this if you want to override default scales for IOS and Android
-  //prompt();
+  // prompt();
+
+  //Experimental crap
+  // var exports = context.exports;
+  //
+  // for (var i=0; i < exports.count(); i++) {
+  //   var currentExport = exports.objectAtIndex(i);
+  //   log("^^^^^^^^^ request format ^^^^^^^^^^^^^^");
+  //   log(currentExport.request.format());
+  // }
 
   for (var i = 0; i < selectedCount; i++) {
     var layer = selectedLayers[i];
     if (i != 0) {
       log("");
     }
-    prepareExportSizes(layer);
+    prepareExportSizes(layer, format);
   }
   context.document.currentPage().deselectAllLayers();
+
+
   log("\nDone.");
 }
 
